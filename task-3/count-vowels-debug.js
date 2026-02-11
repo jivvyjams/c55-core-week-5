d1994df9fbdab5d90d4455a42d3043eb9a037049
Jawad Al Bdiwi <github.2snsr@passmail.com>
2026-02-11 18:53:22 +0100

    assignment complete

--- task-3/count-vowels-debug.js
+++ task-3/count-vowels-debug.js
@@ -1,17 +1,16 @@
 function countVowels(text) {
   let count = 0;
-
-  for (let i = 0; i <= text.length; i++) {
+  const lowerCase = text.toLowerCase();
+  for (let i = 0; i < lowerCase.length; i++) {
     if (
-      text[i] === "a" ||
-      text[i] === "e" ||
-      text[i] === "i" ||
-      text[i] === "o" ||
-      text[i] === "u"
+      lowerCase[i] === "a" ||
+      lowerCase[i] === "e" ||
+      lowerCase[i] === "i" ||
+      lowerCase[i] === "o" ||
+      lowerCase[i] === "u"
     ) {
       count++;
     }
   }
-
   return count;
 }
