093d25e1b726a91e56f1c27eb2edcd920e6ea8fd
Jawad Al Bdiwi <github.2snsr@passmail.com>
2026-02-11 18:51:37 +0100

    assignment complete

--- task-1/count-above-threshold.js
+++ task-1/count-above-threshold.js
@@ -1,1 +1,9 @@
-//Your code here
+function countAboveThreshold(numbers, threshold) {
+  let count = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > threshold) count++;
+  }
+  return count;
+}
+
+console.log(countAboveThreshold([1, 2, 3, 20, 100], 9));
